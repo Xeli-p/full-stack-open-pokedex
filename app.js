1ef3561f2530f249d8bf1c5cd0536970abdaf927
Lauri <lauri.o.pirila@gmail.com>
2024-03-12 14:38:13 +0200

    fifth commit with code fixes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 
 // get the port from env variable
@@ -9,4 +9,4 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
-});
+})

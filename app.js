04613a6e4c590e8f5de5bd8a022780527ffeb2b9
Lauri <lauri.o.pirila@gmail.com>
2024-03-13 13:08:44 +0200

    health check check pt14

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
+app.get('/version', (req, res) => {
+  res.send('1')
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
